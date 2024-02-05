8bfaec6090f098edc0c3e0f866ad3689b2f4dcb9
reelkrey <reelkrey@email.com>
2024-02-05 19:55:39 +0000

    small changes

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -15,7 +15,23 @@ onMounted(async () => {
   <div>
     <div v-if="isLoading">Загрузка...</div>
     <div v-if="packageVersions">
+      <div class="search__params">
+        {{ packageStore.searchParams }}
+      </div>
       <PackageVersions :packageVersions="packageVersions" />
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped§>
+.search__params {
+  margin: 0 auto;
+  padding: 10px 20px;
+  margin-bottom: 20px;
+  border-radius: 30px;
+  max-width: max-content;
+  font-size: 30px;
+  color: #fff;
+  background-color: hsl(201, 64%, 42%);
+}
+</style>
