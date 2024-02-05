7352af0226bad88a775f6de168f45cdf5bbab0f4
reelkrey <reelkrey@email.com>
2024-02-05 19:31:03 +0000

    add some logic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
+  <main>
     <Header />
     <RouterView />
-  </div>
+  </main>
 </template>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, computed } from 'vue'
 import { usePackageStore } from '@/stores/package'
 
 const packageStore = usePackageStore()
+const packageVersions = computed(() => packageStore.packageVersions.versions)
+const isLoading = computed(() => packageStore.packageVersions.loading)
 
 onMounted(async () => {
   await packageStore.getPackage()
@@ -11,6 +13,9 @@ onMounted(async () => {
 
 <template>
   <div>
-    <PackageVersions :packageVersions="packageStore.packageVersions.versions" />
+    <div v-if="isLoading">Загрузка...</div>
+    <div v-if="packageVersions">
+      <PackageVersions :packageVersions="packageVersions" />
+    </div>
   </div>
 </template>
